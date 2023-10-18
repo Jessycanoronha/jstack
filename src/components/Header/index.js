cf35130422b9a562ffc513bf0161885f325120a2
Jessyca Noronha <jessycarozeene@gmail.com>
2023-10-17 23:19:26 -0300

    feat: How to type props

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 import { Container } from "./styles";
 
 export default class Header extends Component {
+  //static faz com que essa prop nao pertenca mais a instancia da classe e sim e classe
+  //static = classe  prop=instancia da classe
+  static propTypes = {
+    selectedTheme: PropTypes.string.isRequired,
+    onToggleTheme: PropTypes.func.isRequired,
+} 
   render() {
     const {onToggleTheme, selectedTheme} = this.props
     return (
@@ -15,11 +21,7 @@ export default class Header extends Component {
     );
   }
 }
-// export default function Header() {
-//   return (
-//     <Container>
-//       <h1>JStack's Blog</h1>
-//       <button type="button">🌞</button>
-//     </Container>
-//   );
-// }
+Header.propTypes = {
+    selectedTheme: PropTypes.string.isRequired,
+    onToggleTheme: PropTypes.func.isRequired,
+} 
