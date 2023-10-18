075262853abdad26da6e932d1672c82e4431a4c9
Jessyca Noronha <jessycarozeene@gmail.com>
2023-10-17 22:59:05 -0300

    refactor: Update code to classComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useMemo, useContext } from "react";
-import { ThemeProvider, StyledThemeProvider } from "styled-components";
+import React from "react";
+import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "./styles/global";
 import Layout from "./components/Layout";
@@ -8,45 +8,26 @@ import themes from "./styles/themes/";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      theme: "dark",
-    };
-    function setState(newState) {
-      this.state = {
-        ...currentState,
-        ...newState,
-      };
-    }
+   
+    this.handleToggleTheme = this.handleToggleTheme.bind(this);
+  }
+  state = {
+    theme: "dark",
+  };
+ 
+  //posso substituir por arrow function, pois ele tem seu próprio objeto this
+  handleToggleTheme() {
+      this.setState(prevState => ({
+        theme: prevState.theme === "dark" ? "light" : "dark",
+    }));
   }
   render() {
     const { theme } = this.state;
-    console.log(this.state)
     return (
       <ThemeProvider theme={themes[theme] || themes.dark}>
-        {/* <StyledThemeProvider theme={themes[theme] || themes.dark}> */}
         <GlobalStyle />
-        <Layout onToggleTheme={()=>{this.setState({theme:'light'})}} selectedTheme={theme} />
-        {/* </StyledThemeProvider> */}
+        <Layout onToggleTheme={this.handleToggleTheme} selectedTheme={theme} />
       </ThemeProvider>
     );
   }
 }
-// export const App = () => {
-//   const [theme, setTheme] = useState("dark");
-//   const currentTheme = useMemo(() => {
-//     themes[theme] = themes['dark'] = themes.theme = themes.dark
-//     return themes[theme] || themes.dark;
-//   }, [theme]);
-//   function handleToggleTheme() {
-//     setTheme((prevState) => (prevState === "dark" ? "light" : "dark"));
-//   }
-
-//   return (
-//     <ThemeProvider>
-//       <StyledThemeProvider theme={currentTheme}>
-//         <GlobalStyle />
-//         <Layout onToggleTheme={handleToggleTheme} selectedTheme={theme} />
-//       </StyledThemeProvider>
-//     </ThemeProvider>
-//   );
-// };
