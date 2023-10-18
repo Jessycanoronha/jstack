c63a3264a58ba515bd9ab9847d31eb152bec9d18
Jessyca Noronha <jessycarozeene@gmail.com>
2023-10-17 23:05:18 -0300

    feat: Learn how to force rendering

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default class App extends React.Component {
       this.setState(prevState => ({
         theme: prevState.theme === "dark" ? "light" : "dark",
     }));
+    this.forceUpdate();
   }
   render() {
     const { theme } = this.state;
